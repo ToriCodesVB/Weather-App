c69bc993d9da92b3c8ecc65f839a8f95497c0a76
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2022-12-30 04:20:32 +0200

    Added search function

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ let minutes = now.getMinutes();
 let day = days[now.getDay()];
 let todayDate = `${day}, ${hours}:${minutes}`;
 currentDate.innerHTML = `${todayDate}`;
+
+function search(event) {
+  event.preventDefault();
+  let currentCity = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  currentCity.innerHTML = cityInput.value;
+
+  console.log(cityInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
